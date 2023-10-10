acd78d4ffecfbfc2aa531ca1aa7eeb021d5eded5
Yogurt_with_thoughts <124450910+yoGurtwithThoughts@users.noreply.github.com>
2023-10-10 22:17:29 +0300

    совместить калькулятор с основной страницей. доделать виджеты магазина. разработать страницы товара, сделать страницу профиля. ПЕРЕДЕЛАНЫ ЦВЕТА И РАЗМЕТКА. доработать карточки виджета добавив текст. сделать менюшку с выбором и текстом.

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -5,161 +5,152 @@ import 'package:mobiledevelop/widgets/stSH.dart';
 class ShPage extends StatelessWidget{
   const ShPage({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(0, 0, 0,55),
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'Добро пожаловать в наш магазин!'
-              'Надеемся что вы найдете нужный товар',
-          style: TextStyle(
-            color: Color.fromARGB(0, 0, 255,1),)
-        ),
-        backgroundColor: Color.fromRGBO(128, 0, 128, 65),
-        systemOverlayStyle: SystemUiOverlayStyle.dark,
-        elevation: 25,
-        leading: IconButton(
+      backgroundColor: Colors.black54,
+    appBar: AppBar(
+    centerTitle: true,
+    title:
+    const Text(
+    'Online store Digital Cat',
+    style: TextStyle(
+    color: Colors.blueAccent,
+    fontSize: 20,
+      fontWeight: FontWeight.w500,
+    ),),
+      backgroundColor: Color.fromRGBO(128, 0, 128, 65),
+      systemOverlayStyle: SystemUiOverlayStyle.dark,
+      elevation: 25,
+      leading: IconButton(
           onPressed: () {},
           icon: const Icon(
             Icons.menu,
-          color:  Colors.blue,)
-        ),
+            color:  Colors.blue,)
       ),
+    ),
       body: SafeArea(
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+    Container(
+    width: double.infinity,
+    decoration: const BoxDecoration(
+    color: Color.fromRGBO(0, 0, 139,85),
+    borderRadius: BorderRadius.horizontal(
+    left: Radius.circular(15),
+    right: Radius.circular(15),
+    ),
+    ),
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        const Text('Лучшие производители комплектующих для ПК',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 15,
+        ),),
+          const SizedBox(
+            height: 5,
+          ),
+          const Text('Выберите товар с лучшими отзывами.',
+            style: TextStyle(
+                color: Color.fromRGBO(224, 255, 255, 1),
+                fontSize: 25,
+                fontWeight: FontWeight.bold
+            ),),
+          const SizedBox(
+            height: 25,
+          ),
+          Container(
+            padding: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(0, 0, 0,1),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                prefixIcon: Icon(Icons.search, color: Colors.blue,
+                    size: 15),
+                hintText: 'Что нужно найти?',
+                hintStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+        ],
+      ),
+    ),
+      const SizedBox(
+        height: 20,
+      ),
+      Padding(padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 25.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Color.fromRGBO(47, 79, 79, 75),
-                borderRadius: BorderRadius.horizontal(
-                  left: Radius.circular(15),
-                  right: Radius.circular(15),
-                )
+            const Text('Лучший товар по мнению покупателей',
+              style: TextStyle(
+                color: Color.fromRGBO(224, 255, 255, 1),
+                fontSize: 15,
               ),
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('Найдется всё',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                  ),),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text('Выберите товар с лучшими отзывами.',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold
-                  ),),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: const Color.fromRGBO(128, 0, 0, 5),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Icon(Icons.search, color: Color.fromRGBO(139, 0, 0, 1),
-                        size: 5),
-                        hintText: 'Что нужно найти?',
-                        hintStyle: TextStyle(
-                          color: Colors.white,
-                          fontSize: 10,
-                        ),
-                        ),
-                      ),
-                    ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                ],
-              ),
-              ),
+            ),
             const SizedBox(
               height: 20,
             ),
-            Padding(padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 25.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Лучший товар по мнению покупателей',
-                  style: TextStyle(
-                    color: Color.fromRGBO(224, 255, 255, 1),
-                    fontSize: 15,
-                  ),
-
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
-                  height: 200,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                  /* promoCardWidget('assets/corp.jpg'),
+            SizedBox(
+              height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  promoCardWidget('assets/corp.jpg'),
                     promoCardWidget('assets/cr9i.jpg'),
                     promoCardWidget('assets/vicard.jpg'),
                     promoCardWidget('assets/deepcl.jpg'),
 
-                 */ ],
-                ),),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                    image: const DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage('assets/images/logO.')
-                    ),
-                  ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25.5),
-                      gradient: LinearGradient(
-                        begin: Alignment.topRight,
-                        stops: const [0.3,0.9],
-                        colors: [Colors.pink.withOpacity(56.6),
-                        Colors.red.withOpacity(55),],
-                      ),
-                    ),
-                    child: const Align(
-                      alignment: Alignment.bottomRight,
-                      child:
-                      Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Text('Выберите категорию',
-                          style: TextStyle(
-                            color: Color.fromRGBO(255, 215, 0, 1),
-                          ),
-                        ),
-                      ),
-                    ),
+                 ],
+              ),),
 
+          ],
 
-
-                  ),
-                   ),],
-
-             ),
-            ),
-      ],
-
-        ),
       ),
+
+    ),
+
+    ],),
+    )
     );
   }
-}
+
+  promoCardWidget(image) {
+    return AspectRatio(aspectRatio: 2.62/3,
+    child: Container(
+        margin: const EdgeInsets.only(right: 15),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      image: DecorationImage(
+        fit: BoxFit.cover,
+        image: AssetImage(image,)
+      ),
+    ),
+    child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        gradient: LinearGradient(
+          begin: Alignment.bottomRight,
+          stops: const[0.1,0.9],
+          colors: [
+            Color.fromRGBO(0, 0, 0, 1),
+          ]
+        ),
+      ),
+    ),
+    ),
+    );
+  }
+  }
--- lib/widgets/stSH.dart
+++ lib/widgets/stSH.dart
@@ -1,30 +1,30 @@
 import 'package:flutter/material.dart';
-class Widgetfr extends StatefulWidget{
-  final String varSh;
-  final bool isvr;
+class prasL extends StatelessWidget{
+  final String Shvar;
+  final bool isS;
   final VoidCallback onTp;
-  const Widgetfr (
+  const prasL(
       {
-        required this.varSh,
-        required this.isvr,
-        super.key, required this.onTp
-      });
+        required this.Shvar,
+        required this.onTp,
+        super.key,
+        required this.isS});
   @override
   Widget build(BuildContext context){
     return GestureDetector(
-        onTap: onTp,
-        child: Padding(
+      onTap: onTp,
+      child: Padding(
         padding: const EdgeInsets.only (left: 25.0),
-    child: Text(
-    varSh,
-    style: TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.bold,
-    color: isvr? Colors.lightGreenAccent : Colors.limeAccent),
-    ),
-    ),
+        child: Text(
+          Shvar,
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: isS? Colors.blue : Colors.lightBlueAccent),
+
+        ),
+      ),
     );
+
   }
-
 }
-
